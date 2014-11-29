2e02c4b3714e75e24ac91e6a0197bc3ed3416863
Erik Nilsen <enilsen16@live.com>
2014-11-29 14:19:09 -0800

    getting closer

--- check_braces.rb
+++ check_braces.rb
@@ -5,21 +5,20 @@ def check_braces(expressions)
   value = hash.values
   expressions.each do |set|
     set.each_char do |char|
-    if value.include? char && stack.empty?
-      puts 0
-      next
-    end
       if key.include? char
         stack << char
       elsif value.include? char
-        if hash[stack.pop] != char
+        if stack.empty?
+          puts 0
+          break
+        elsif hash[stack.pop] != char
           puts 0
           break
         end
       end
     end
-    puts 1 if stack.empty? # rather than check for the zero cases, I need to check for the "puts 1" scenario
+    puts 1 if stack.empty?
   end
 end
 
-check_braces( ["(())","[]({})","([])","{()[]}","([)]", "[])"] )
+check_braces( ["(())","[)]({})","([])","{()[]}","([)]", "[]))"] )

163622ee899fb9596d146bebe4dd78c314fb3de2
Erik Nilsen <enilsen16@live.com>
2014-11-30 23:42:24 -0800

    havng a count doesnt seem to work either

--- check_braces.rb
+++ check_braces.rb
@@ -1,10 +1,12 @@
 def check_braces(expressions)
   stack = []
+  count = 0
   hash = { '[' => ']', '(' => ')', '{' => '}' }
   key = hash.keys
   value = hash.values
   expressions.each do |set|
     set.each_char do |char|
+      count += 1
       if key.include? char
         stack << char
       elsif value.include? char
@@ -17,8 +19,9 @@ def check_braces(expressions)
         end
       end
     end
-    puts 1 if stack.empty?
+    puts 1 if stack.empty? && set.length == count
+    count = 0
   end
 end
 
-check_braces( ["(())","[)]({})","([])","{()[]}","([)]", "[]))"] )
+check_braces( ["(())","[)]({})","{()[]}","([)]", "[]))"] )
